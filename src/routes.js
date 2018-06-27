2b5ae8cb8bbfa9a66a17b335131f031881bea0dc
danielvportlandia <danielshelton90@gmail.com>
2018-06-27 16:54:21 -0700

    scaffolding complete

---
+++ src/routes.js
@@ -0,0 +1,5 @@
+export const LANDING_ROUTE = '/';
+export const ABOUT_ROUTE = '/about';
+export const CONTACT_ROUTE = '/contact';
+export const PROJECTS_ROUTE = '/projects';
+export const RESUME_ROUTE = '/resume';
