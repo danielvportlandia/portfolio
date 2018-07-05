60ef577338d40fc021072c53ef2627ab7e2805c3
danielvportlandia <danielshelton90@gmail.com>
2018-07-05 15:07:33 -0700

    Added image assets and images to landing.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,6 +6,8 @@ export default class Header extends React.Component {
   render() {
     return (
       <header>
+        <h1 className="title">Daniel Shelton</h1>
+        <h2 className="subTitle">Full-Stack JavaScript Developer</h2>
         <nav>
           <li><Link to='/'>HOME</Link></li>
           {/* <li><Link to='/about'>ABOUT</Link></li>
--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import discGolfImg from '../../assets/disc-golf.jpg';
+import writingImg from '../../assets/writing.jpg';
+import codingImg from '../../assets/programming.jpg';
 
 export default class Landing extends React.Component {
   render() {
     return (
       <div className='landing'>
-        <h1>Home</h1>
-        <h2> if you can see this, then you are logged in...</h2>
+        <p>
+          Daniel Shelton is a full stack developer in Seattle, Washington. Passionate about...
+        </p>
+        <div className='image-container'>
+          <img src={writingImg} />
+          <h2 className='image-text'>Writing</h2>
+        </div>
+        <div className='image-container'>
+          <img src={discGolfImg} />
+          <h2 className='image-text'>Disc Golf</h2>
+        </div>
+        <div className='image-container'>
+          <img src={codingImg} />
+          <h2 className='image-text'>Programming</h2>
+        </div>
       </div>
     );
   }
