42707c9133409e73faf328bac59d8351bf08718a
danielvportlandia <danielshelton90@gmail.com>
2018-07-05 16:28:27 -0700

    comparing master

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,13 +6,17 @@ export default class Header extends React.Component {
   render() {
     return (
       <header>
+        <nav className='nav-left'>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/redeeming-soles-project'>Redeeming Soles</Link></li>
+          <li><Link to='/bloomio-project'>Bloomio</Link></li>
+        </nav>
         <h1 className="title">Daniel Shelton</h1>
         <h2 className="subTitle">Full-Stack JavaScript Developer</h2>
-        <nav>
-          <li><Link to='/'>HOME</Link></li>
-          {/* <li><Link to='/about'>ABOUT</Link></li>
-          <li><Link to='/projects'>Projects</Link></li>
-          <li><Link to='/contact'>CONTACT</Link></li> */}
+        <nav className='nav-right'>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/qualifications'>Qualifications</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
         </nav>
       </header>
     );
