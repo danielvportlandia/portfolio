43906c154dc59db0e8263f418dac21f7e6ac850b
danielvportlandia <danielshelton90@gmail.com>
2018-07-08 16:00:26 -0700

    background page complete, added buttons to resume page.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Favicon from 'react-favicon';
 
+import favicon from '../../assets/my-favicon.png';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import Landing from '../landing/landing';
 import Resume from '../resume/resume';
-import favicon from '../../assets/my-favicon.png';
 import ROUTES from '../../routes';
 import About from '../about/about';
+import BackGround from '../background/background';
+import Skills from '../skills/skills';
 // import Contact from '../contact/contact';
 // import RedeemingSolesProject from '../redeeming-soles-project/redeeming-soles-project';
 // import BloomioProject from '../bloomio-project/bloomio-project';
@@ -26,6 +28,8 @@ export default class App extends React.Component {
             <Route exact path={ROUTES.LANDING} component={Landing}/>
             <Route exact path={ROUTES.RESUME} component={Resume}/>
             <Route exact path={ROUTES.ABOUT} component={About}/>
+            <Route exact path={ROUTES.BACKGROUND} component={BackGround}/>
+            <Route exact path={ROUTES.SKILLS} component={Skills}/>
             {/* <Route exact path="/contact" component={Contact}/>
             <Route exact path="/redeeming-soles-project" component={RedeemingSolesProject}
             />
