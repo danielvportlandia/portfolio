58531986da3a5dc359febf1b82564ebacf8c238c
danielvportlandia <danielshelton90@gmail.com>
2018-07-11 19:39:10 -0700

    contact page complete

--- src/components/redeeming-soles-project/redeeming-soles-project.js
+++ src/components/redeeming-soles-project/redeeming-soles-project.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ROUTES from '../../routes';
 
+// import RSBannerImg from '../../assets/rs-banner.jpg';
 import './redeeming-soles-project.scss';
 
 class RSProject extends React.Component {
   render() {
     return (
       <div className='rs-project-page'>
-        <h1>Redeeming Soles</h1>
+        <p className='hero-text'>Redeeming Soles</p>
+        <div className='banner-wrapper'>
+          <h2>No KIDS LEFT ON THE SIDELINES</h2>
+          {/* <img src={RSBannerImg}/> */}
+        </div>
         <Link to={ROUTES.BLOOMIO_PROJECT}>
           <button className='raise'>SEE NEXT PROJECT</button>
         </Link>
--- src/routes.js
+++ src/routes.js
@@ -10,13 +10,3 @@ const ROUTES = {
 };
 
 export default ROUTES;
-
-// export const LANDING_ROUTE = '/';
-// export const ABOUT_ROUTE = '/about';
-// export const RS_PROJECT_ROUTE = '/redeeming-soles-project';
-// export const BLOOMIO_PROJECT_ROUTE = '/bloomio-project';
-// export const BACKGROUND_ROUTE = '/background';
-// export const EDUCATION_ROUTE = '/education';
-// export const SKILLS_ROUTE = '/skills';
-// export const RESUME_ROUTE = '/resume';
-// export const CONTACT_ROUTE = '/contact';
