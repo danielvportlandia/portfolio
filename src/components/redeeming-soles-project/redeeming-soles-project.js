711a8ba45fd870069bc7aaf6548eea1781ce91ac
danielvportlandia <danielshelton90@gmail.com>
2018-07-11 20:00:03 -0700

    RS banner added.

--- src/components/redeeming-soles-project/redeeming-soles-project.js
+++ src/components/redeeming-soles-project/redeeming-soles-project.js
@@ -2,18 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ROUTES from '../../routes';
 
-// import RSBannerImg from '../../assets/rs-banner.jpg';
 import './redeeming-soles-project.scss';
 
 class RSProject extends React.Component {
   render() {
     return (
       <div className='rs-project-page'>
-        <p className='hero-text'>Redeeming Soles</p>
         <div className='banner-wrapper'>
-          <h2>No KIDS LEFT ON THE SIDELINES</h2>
-          {/* <img src={RSBannerImg}/> */}
+          <h2>No Kids Left On The Sidelines</h2>
+          <h3>A REDEEMING SOLES INITIATIVE</h3>
         </div>
+        <p className='hero-text'>A Redeeming Soles Initiative</p>
         <Link to={ROUTES.BLOOMIO_PROJECT}>
           <button className='raise'>SEE NEXT PROJECT</button>
         </Link>
