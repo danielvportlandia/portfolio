e336d09fac3b8df19acd32a1a015bc623024f962
danielvportlandia <danielshelton90@gmail.com>
2018-07-13 19:32:28 -0700

    Changes to Index.js, and webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,7 @@ webpackConfig.entry = `${__dirname}/src/main.js`;
 webpackConfig.output = {
   filename: '[name].[hash].js',
   path: `${__dirname}/build`,
-  publicPath: process.env.CDN_URL,
+  // publicPath: process.env.CDN_URL,
 };
 
 webpackConfig.plugins = [
