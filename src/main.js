35094f7b009939a0ab583b42ba8ad4ffd25cec5b
danielvportlandia <danielshelton90@gmail.com>
2018-07-18 17:09:37 -0700

    Added react-snap package, changed build script and main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 'use strict';
 
 import React from 'react';
-import { render } from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import App from './components/app/app';
 
 const app = document.createElement('div');
 document.body.appendChild(app);
-render(<App/>, app);
+
+if (app.hasChildNodes()) {
+  hydrate(<App/>, app);
+} else {
+  render(<App/>, app);
+}
