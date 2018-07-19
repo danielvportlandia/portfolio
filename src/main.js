6110d9d5faccf9d383af9c55eee3510b10351302
danielvportlandia <danielshelton90@gmail.com>
2018-07-18 17:22:40 -0700

    Removing react-snap package

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 'use strict';
 
 import React from 'react';
-import { hydrate, render } from 'react-dom';
+import { render } from 'react-dom';
 import App from './components/app/app';
 
 const app = document.createElement('div');
 document.body.appendChild(app);
-
-if (app.hasChildNodes()) {
-  hydrate(<App/>, app);
-} else {
-  render(<App/>, app);
-}
+render(<App/>, app);
