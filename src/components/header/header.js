da454227bd0306fd2e9ccb9879c1dfa0ce0b5452
danielvportlandia <danielshelton90@gmail.com>
2018-07-22 01:45:21 -0700

    Sorted SVGs into own folder

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 // -------------------------------------------------------------------------------------------------
 // SVG IMPORTS
 // -------------------------------------------------------------------------------------------------
-import HomeSVG from '../../icomoon/SVG/home.svg';
-import AboutMeSVG from '../../icomoon/SVG/user-tie.svg';
-import CaretSVG from '../../icomoon/SVG/play3.svg';
-import ResumeSVG from '../../icomoon/SVG/profile.svg';
-import ContactSVG from '../../icomoon/SVG/address-book.svg';
-import RS_SVG from '../../icomoon/SVG/gift.svg';
-import LeafSVG from '../../icomoon/SVG/leaf.svg';
-import MedicalSVG from '../../icomoon/SVG/aid-kit.svg';
-import EducationSVG from '../../icomoon/SVG/embed2.svg';
-import HamburgerMenuSVG from '../../icomoon/SVG/menu.svg';
+import HomeSVG from '../../lib/icomoon/home.svg';
+import AboutMeSVG from '../../lib/icomoon/user-tie.svg';
+import CaretSVG from '../../lib/icomoon/play3.svg';
+import ResumeSVG from '../../lib/icomoon/profile.svg';
+import ContactSVG from '../../lib/icomoon/address-book.svg';
+import RS_SVG from '../../lib/icomoon/gift.svg';
+import LeafSVG from '../../lib/icomoon/leaf.svg';
+import MedicalSVG from '../../lib/icomoon/aid-kit.svg';
+import EducationSVG from '../../lib/icomoon/embed2.svg';
+import HamburgerMenuSVG from '../../lib/icomoon/menu.svg';
 
 import './header.scss';
 
@@ -23,18 +23,18 @@ export default class Header extends React.Component {
       <header className='topnav'>
         <nav className='nav-left'>
           <div className='nav-button-container'>
-            <button className='link-button'><Link to='/'>Home <HomeSVG className='icomoon-svgs'/></Link></button>
+            <button className='link-button'><Link to='/'>Home <HomeSVG className='icomoon-svgs wiggle'/></Link></button>
           </div>
           <div className='nav-button-container'>
-            <button className='link-button'><Link to='/about'>About <AboutMeSVG className='icomoon-svgs'/></Link></button>
+            <button className='link-button'><Link to='/about'>About <AboutMeSVG className='icomoon-svgs wiggle'/></Link></button>
           </div>
           <div className='dropdown'>
             <button className='dropbutton'>
               Projects <CaretSVG className='icomoon-svgs caret'/>
             </button>
             <div className='dropdown-list'>
-              <Link to='/redeeming-soles-project'>Redeeming Soles <RS_SVG className='icomoon-svgs'/></Link>
-              <Link to='/bloomio-project'>Bloomio <LeafSVG className='icomoon-svgs'/></Link>
+              <Link to='/redeeming-soles-project'>Redeeming Soles <RS_SVG className='icomoon-svgs wiggle'/></Link>
+              <Link to='/bloomio-project'>Bloomio <LeafSVG className='icomoon-svgs wiggle'/></Link>
             </div>
           </div>
         </nav>
@@ -47,15 +47,15 @@ export default class Header extends React.Component {
               Qualifications <CaretSVG className='icomoon-svgs caret'/>
             </button>
             <div className='dropdown-list'>
-              <Link to='/background'>Background <MedicalSVG className='icomoon-svgs'/></Link>
-              <Link to='/education'>Education <EducationSVG className='icomoon-svgs'/></Link>
+              <Link to='/background'>Background <MedicalSVG className='icomoon-svgs wiggle'/></Link>
+              <Link to='/education'>Education <EducationSVG className='icomoon-svgs wiggle'/></Link>
             </div>
           </div>
           <div className='nav-button-container'>
-            <button className='link-button'><Link to='/resume'>Resume <ResumeSVG className='icomoon-svgs'/></Link></button>
+            <button className='link-button'><Link to='/resume'>Resume <ResumeSVG className='icomoon-svgs wiggle'/></Link></button>
           </div>
           <div className='nav-button-container'>
-            <button className='link-button'><Link to='/contact'>Contact <ContactSVG className='icomoon-svgs'/></Link></button>
+            <button className='link-button'><Link to='/contact'>Contact <ContactSVG className='icomoon-svgs wiggle'/></Link></button>
           </div>
         </nav>
 
