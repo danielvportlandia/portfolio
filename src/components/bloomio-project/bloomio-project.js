c254ca0af540fdc91c8aa2c4ca1f50254ec10a7a
danielvportlandia <danielshelton90@gmail.com>
2018-08-18 19:05:57 -0700

    scrolling effects added to bloomio page

--- src/components/bloomio-project/bloomio-project.js
+++ src/components/bloomio-project/bloomio-project.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
+import Flip from 'react-reveal/Flip';
 
 import bloomioDiagramImg from '../../assets/bloomio-schema-diagram.jpg';
 import bloomioTestingImg from '../../assets/bloomio-testing.jpg';
@@ -51,28 +53,35 @@ class BloomioProject extends React.Component {
 
         <section>
           <article>
-            <h4>Idea Origins <IdeaSVG className='icomoon-svgs'/></h4>
+            <Fade top className='reveals'>
+              <h4>Idea Origins <IdeaSVG className='icomoon-svgs'/></h4>
+            </Fade>
             <div className='article-details-wrapper'>
             
               <div className='article-text-wrapper'>
-                <p className='article-text'>
-                  Living in apartments most of my life I have always had house plants, to not only 
-                  clean the air in the apartment but also to spruce them up a bit, however, I never 
-                  did have much luck keeping my plants alive (<strong>except for my 
-                  cacti</strong>). My plants often died from either being over watered or not 
-                  watered enough. Even though I could keep track of their watering schedules with a 
-                  calendar,  it was still a lot of work to manually log which plants to water and 
-                  when and to figure out when their next watering date should be. <br/>
-                  <br/>
-                  So that&apos;s where the idea for <strong>Bloomio</strong> originated. Bloomio 
-                  was the answer to helping others manage their plants and keept them happy and 
-                  alive.
-                </p>
+                <Fade left className='reveals'>
+                  <p className='article-text'>
+                    Living in apartments most of my life I have always had house plants, to 
+                    not only clean the air in the apartment but also to spruce them up a bit, 
+                    however, I never did have much luck keeping my plants alive (<strong>except 
+                    for my cacti</strong>). My plants often died from either being over watered 
+                    or not watered enough. Even though I could keep track of their watering 
+                    schedules with a calendar,  it was still a lot of work to manually log which 
+                    plants to water and when and to figure out when their next watering date should 
+                    be. <br/>
+                    <br/>
+                    So that&apos;s where the idea for <strong>Bloomio</strong> originated. Bloomio 
+                    was the answer to helping others manage their plants and keept them happy and 
+                    alive.
+                  </p>
+                </Fade>
               </div>
 
               <div className='images-wrapper'>
                 <figure>
-                  <img src={bloomioCactiImg}/>
+                  <Flip top className='reveals'>
+                    <img src={bloomioCactiImg}/>
+                  </Flip>
                 </figure>
               </div>
 
@@ -82,35 +91,41 @@ class BloomioProject extends React.Component {
 
         <section>
           <article>
-            <h4>Planning <PlanningSVG className='icomoon-svgs'/></h4>
+            <Fade top className='reveals'>
+              <h4>Planning <PlanningSVG className='icomoon-svgs'/></h4>
+            </Fade>
             <div className='article-details-wrapper'>
 
               <div className='article-text-wrapper'>
-                <p className='article-text'>
-                  <strong>User Stories:</strong><br/>
-                  <UserSVG className='icomoon-svgs'/> I want to be able to login to my account. <br/>
-                  <UserSVG className='icomoon-svgs'/> I want to be able to upload a profile pic. <br/>
-                  <UserSVG className='icomoon-svgs'/> I want to be able to add, edit, and remove plants in my collection. <br/>
-                  <UserSVG className='icomoon-svgs'/> I want to be notified via text if I have plants that require care. <br/>
-                  <UserSVG className='icomoon-svgs'/> I want to be able to find plants in the database with care instructions. <br/>
-                </p>
-                <p className='article-text'>
-                  <strong>Wireframing:</strong><br/>
-                  Working off of the user stories, my team and I put together our wireframing.
-                </p>
-                <p className='article-text'>
-                  <strong>MVP:</strong><br/>
-                  As we scoped the project, some desired functionality such as fertilizer reminders 
-                  and having a well-detailed master data base of plants were considered not 
-                  attainable within our week-long sprint and thus pushed into phase 2 of this 
-                  project.
-                </p>
+                <Fade left className='reveals'>
+                  <p className='article-text'>
+                    <strong>User Stories:</strong><br/>
+                    <UserSVG className='icomoon-svgs'/> I want to be able to login to my account. <br/>
+                    <UserSVG className='icomoon-svgs'/> I want to be able to upload a profile pic. <br/>
+                    <UserSVG className='icomoon-svgs'/> I want to be able to add, edit, and remove plants in my collection. <br/>
+                    <UserSVG className='icomoon-svgs'/> I want to be notified via text if I have plants that require care. <br/>
+                    <UserSVG className='icomoon-svgs'/> I want to be able to find plants in the database with care instructions. <br/>
+                  </p>
+                  <p className='article-text'>
+                    <strong>Wireframing:</strong><br/>
+                    Working off of the user stories, my team and I put together our wireframing.
+                  </p>
+                  <p className='article-text'>
+                    <strong>MVP:</strong><br/>
+                    As we scoped the project, some desired functionality such as fertilizer 
+                    reminders and having a well-detailed master data base of plants were considered 
+                    not attainable within our week-long sprint and thus pushed into phase 2 of this 
+                    project.
+                  </p>
+                </Fade>
               </div>
 
               <div className='images-wrapper'>
                 <figure>
-                  <img src={bloomioDiagramImg}/>
-                  <figcaption>Application architecture wireframing</figcaption>
+                  <Flip top className='reveals'>
+                    <img src={bloomioDiagramImg}/>
+                    <figcaption>Application architecture wireframing</figcaption>
+                  </Flip>
                 </figure>
               </div>
 
@@ -120,20 +135,26 @@ class BloomioProject extends React.Component {
 
            <section>
           <article>
-            <h4>Testing <DebugSVG className='icomoon-svgs'/></h4>
+            <Fade top className='reveals'>
+              <h4>Testing <DebugSVG className='icomoon-svgs'/></h4>
+            </Fade>
             <div className='article-details-wrapper'>
 
               <div className='article-text-wrapper'>
-                <p className='article-text'>
-                  <strong>Back-End Testing:</strong><br/>
-                  Unit testing performed via Jest with over 90% in line-coverage.
-                </p>
+                <Fade left className='reveals'>
+                  <p className='article-text'>
+                    <strong>Back-End Testing:</strong><br/>
+                    Unit testing performed via Jest with over 90% in line-coverage.
+                  </p>
+                </Fade>
               </div>
 
               <div className='images-wrapper'>
                 <figure>
-                  <img src={bloomioTestingImg}/>
-                  <figcaption>Back-End Testing Results</figcaption>
+                  <Flip top className='reveals'>
+                    <img src={bloomioTestingImg}/>
+                    <figcaption>Back-End Testing Results</figcaption>
+                  </Flip>
                 </figure>
               </div>
 
@@ -143,39 +164,50 @@ class BloomioProject extends React.Component {
 
         <section>
           <article>
-          <h4>Project Details <PrototypeSVG className='icomoon-svgs'/></h4>
+          <Fade top className='reveals'>
+            <h4>Project Details <PrototypeSVG className='icomoon-svgs'/></h4>
+          </Fade>
             <div className='article-details-wrapper'>
 
               <div className='article-text-wrapper'>
-                <p>
-                  <strong>Collaboration:</strong><br/>
-                  After a thorough planning phase, we scaffolded the project and pair programmed to 
-                  create all the necessary schemas of the project. Testing was done alongside 
-                  development with a &quot;red green&quot; approach.
-                </p>
-                <p>
-                  <strong>External API Integration:</strong><br/>
-                  With our schemas in place, we began the process of integrating external APIs into 
-                  the project. The first being AWS&apos;s S3 for image hosting, the second, was 
-                  Twilio for sending daily plant notifications to the user at a preferred time.
-                </p>
+                <Fade top cascade className='reveals'>
+                  <h5>Collaboration:</h5>
+                  <p>
+                    After a thorough planning phase, we scaffolded the project and pair 
+                    programmed to create all the necessary schemas of the project. Testing 
+                    was done alongside development with a &quot;red green&quot; approach.
+                  </p>
+                </Fade>
+
+                  <h5>External API Integration:</h5>
+                  <p>
+                    With our schemas in place, we began the process of integrating external 
+                    APIs into the project. The first being AWS&apos;s S3 for image hosting, the 
+                    second, was Twilio for sending daily plant notifications to the user at a 
+                    preferred time.
+                  </p>
               </div>
 
               <div className='article-text-wrapper'>
-                <p>
-                  <strong>Problem Encountered:</strong><br/>
-                    One problem we encountered in this project was trying to manage the next water 
-                    date functionality, as dates proved to be tricky to deal with, after some 
-                    research, we implemented <strong> Moment.js</strong> to resolve this problem 
-                    quickly.
-                </p>
-                <p>
-                  <strong>What I learned:</strong><br/>
-                    How to manage web pre-hooks and post-hooks with schemas <br/>
-                    how to effectively test with Jest. <br/>
-                    How to connect applications to <strong>AWS&apos;s S3</strong> for image hosting 
-                    and <strong>Twilio</strong> for providing text-based services.
-                </p>
+                <Fade top className='reveals'>
+                  <h5>Problem Encountered:</h5>
+                  <p>
+                      One problem we encountered in this project was trying to manage the 
+                      next water date functionality, as dates proved to be tricky to deal with, 
+                      after some research, we implemented <strong> Moment.js</strong> to resolve 
+                      this problem quickly.
+                  </p>
+                </Fade>
+
+                <Fade top>
+                  <h5>What I learned:</h5>
+                  <p>
+                      How to manage web pre-hooks and post-hooks with schemas <br/>
+                      how to effectively test with Jest. <br/>
+                      How to connect applications to <strong>AWS&apos;s S3</strong> for image 
+                      hosting and <strong>Twilio</strong> for providing text-based services.
+                  </p>
+                </Fade>
               </div>
 
             </div>
